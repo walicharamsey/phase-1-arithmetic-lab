5e2fbbd24607da4b74a4c22101195826eeb1230b
walicharamsey <ramsey@Cullen-PC.localdomain>
2023-10-04 10:28:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 64;
+const num4 = 60;
+const mod = num3 % num4;
+const max = Math.max(1, 2, 3, 4, 20);
+
+console.log("multiply:", multiply);
+console.log("random:", random);
+console.log("mod:", mod);
+console.log("max:", max);
